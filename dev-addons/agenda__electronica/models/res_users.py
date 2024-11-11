4b937b880e2a8751022073354c944f2607c48385
Gustavo Camargo <gusta25demarzo@gmail.comgit config --global user.name Tu>
2024-11-11 13:41:25 -0400

    rols

--- dev-addons/agenda__electronica/models/res_users.py
+++ dev-addons/agenda__electronica/models/res_users.py
@@ -4,3 +4,10 @@ class ResUsers(models.Model):
     _inherit = 'res.users'
 
     device_token_ids = fields.One2many('user.device.token', 'user_id', string="Device Tokens")
+    role_ids = fields.Many2many(
+        'roles.role',         # Modelo relacionado
+        'role_user_rel',       # Nombre de la tabla intermedia
+        'user_id',             # Columna de usuario
+        'role_id',             # Columna de rol
+        string='Roles'         # Etiqueta para mostrar en la interfaz
+    )
