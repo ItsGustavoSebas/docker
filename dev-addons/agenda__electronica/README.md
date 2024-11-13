50846bf1f549dc6c89489991da2d7d36ce1adff3
Gustavo Camargo <gusta25demarzo@gmail.comgit config --global user.name Tu>
2024-11-12 23:08:57 -0400

    market

---
+++ dev-addons/agenda__electronica/README.md
@@ -0,0 +1,5 @@
+# Instalación de Dependencias de Python
+Este módulo requiere algunas dependencias de Python adicionales. Para instalarlas, ejecuta:
+
+```bash
+pip3 install -r requirements.txt
--- dev-addons/agenda__electronica/__manifest__.py
+++ dev-addons/agenda__electronica/__manifest__.py
@@ -1,32 +1,26 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Agenda_Electronica",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
+    'summary': "Agenda escolar para comunicación entre padres, profesores y estudiantes.",
     'description': """
-Long description of module's purpose
+Agenda electrónica académica que permite la comunicación entre padres, estudiantes, y profesores. Proyecto para el segundo parcial de Ingeniería de Software 1 1-2024 FICCT
     """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
+    'author': "Gustavo Camargo, Alex Roman y Carlos Romagera",
+    'website': "https://www.uagrm.edu.bo/",
+    'category': 'Education',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
-    'depends': ['base', 'web'],
+    'license': 'LGPL-3',  
 
-    # always loaded
+    'depends': ['base', 'web', 'contacts', 'calendar', 'account'],
+
+
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
         'views/roles_views.xml',
-        'views/permisos_views.xml',        
+        'views/permisos_views.xml',
         'views/estudiantes_views.xml',
         'views/docente_views.xml',
         'views/administrativo_views.xml',
@@ -39,7 +33,6 @@ Long description of module's purpose
         'views/actividades_views.xml',
     ],
 
-    # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
@@ -47,4 +40,3 @@ Long description of module's purpose
     'installable': True,
     'application': True,
 }
-
